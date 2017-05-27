cfafe5d469f2d8bc55e1d76d1c3fee010d5b8569
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2017-05-27 23:25:36 +0200

    first mini navigation

---
+++ src/Home.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import {Button} from 'react-bootstrap'
+
+const Home = () => (
+
+    <div id="titlle" classID="container">
+        <div>
+            <h2>Home</h2>
+        </div>
+<div id="modal">
+
+
+
+<div id="signup">
+    <h4>Zarejestuj się</h4>
+    <Button>SignUp</Button>
+
+</div>
+    <div>
+        <h4>Zaloguj się</h4>
+        <Button>SignIn</Button>
+
+    </div>
+
+</div>
+
+
+
+    </div>
+)
+
+export default Home
+
+
+
+
