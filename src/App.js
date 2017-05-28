b12c7c5003bf607b1f7ccfae15d31af48ca2da2b
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2017-05-28 12:32:54 +0200

    fix LinkContainer add update json-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-    import React from 'react'
+import React from 'react'
 import {
     BrowserRouter as Router,
     Route,
@@ -6,6 +6,7 @@ import {
 import {
     Nav, Navbar, NavItem,
 } from 'react-bootstrap'
+import {LinkContainer} from 'react-router-bootstrap'
 
 
 import Home from './Home'
@@ -16,28 +17,34 @@ import SingIn from './Singin'
 const WebbscyApp = () => (
     <Router>
         <div>
-
-
             <Navbar>
-                        <Navbar.Header>
-                          <Navbar.Brand>
-                            <a href="#">React-Bootstrap</a>
-                          </Navbar.Brand>
-                        </Navbar.Header>
-                        <Nav>
-                         <NavItem  href="/">Home</NavItem>
-                            <NavItem  href="/about">About</NavItem>
-                            <NavItem  href="/singin">SingIn</NavItem>
-                        </Nav>
-                      </Navbar>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#">React-Bootstrap</a>
+                    </Navbar.Brand>
+                </Navbar.Header>
+                <Nav>
 
+                    <LinkContainer to="/">
+                        <NavItem>Home</NavItem>
+                    </LinkContainer>
+
+                    <LinkContainer to="/about">
+                        <NavItem>About</NavItem>
+                    </LinkContainer>
+
+                    <LinkContainer to="/signin">
+                        <NavItem>SingIn</NavItem>
+                    </LinkContainer>
+                </Nav>
+            </Navbar>
 
 
             <hr/>
 
             <Route exact path="/" component={Home}/>
             <Route path="/about" component={About}/>
-           <Route path="/singin" component={SingIn}/>
+            <Route path="/signin" component={SingIn}/>
         </div>
     </Router>
 )
