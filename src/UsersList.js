718ad7007b1f7665efdaab439f85732e070ba4c8
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2017-06-29 08:54:52 +0200

    add state for userlist

---
+++ src/UsersList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+class UsersList extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            userlist: []
+        }
+
+
+    }
+
+}
+
+export default UsersList
