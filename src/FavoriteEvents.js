42a216dd062ab192ee4119d35fc8bfa829c4bb59
Adam Grabarczyk <adamgrabarczyk.gda@wp.pl>
2017-06-29 23:38:38 +0200

    add remove-from-favs -icon and style

--- src/FavoriteEvents.js
+++ src/FavoriteEvents.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-
 const FavoriteEvents = props => (
 
     <span className="favorite">
@@ -15,7 +14,11 @@ const FavoriteEvents = props => (
                         name: event.Name
                     })
                 ).map(
-                    event => <li className="favorite-event"><Link to={'/calendar/' + event.id}>{event.name}</Link><button onClick={() => props.remove(event.id)}>-</button> </li>
+                    event => <li className="favorite-event">
+                        <Link to={'/calendar/' + event.id}>{event.name}</Link>
+                        <img onClick={() => props.remove(event.id)}
+                             src={"/Icon-remove.ico"}
+                             className="remove-btn"/></li>
                 )
             }
 
